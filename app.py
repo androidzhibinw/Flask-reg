f7d466fe9cb8fbba3184557e7143411c032d32b4
androidzhibinw <android_zhibinw@163.com>
2016-01-05 15:58:49 +0800

    cookie.
    Change-Id: I59f1d83d36b8a5437515396f7cda01def5e8ad6f

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,make_response,request
 app = Flask(__name__)
 @app.route('/')
 def index():
+    #check cookie, if not set render register
+    getip()
+    username = request.cookies.get('username')
+    if not username:
         return render_template('reg.html')
+    else:
+        return  render_template('home.html')
 
+def genSecCode(number):
+    return '123456'
 
+@app.route('/getcode')
+def getcode():
+    #gen security code , insert to db
+    return genSecCode('10086')
+def getip():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+    print ip
 
-
+@app.route('/login')
+def login():
+    #check phonenumber with security code.
+    resp = make_response(redirect('/'))
+    resp.set_cookie('username', 'myname')
+    #set cookie
+    return resp
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0',debug=True)
