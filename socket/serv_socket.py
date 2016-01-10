41941ba54a7bf298c33c3395b313a55265f2744b
zhibinw <android_zhibinw@163.com>
2016-01-10 16:14:38 +0800

    msg queue to send msg to worker thread.

--- socket/serv_socket.py
+++ socket/serv_socket.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python 
 import socket,time
+from threading import Thread
+import Queue
 
+WORK_Q = Queue.Queue()
 def create_server_socket():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
@@ -10,11 +13,22 @@ def create_server_socket():
     #only listen to one 
     con,addr = s.accept()
     return con,addr
+def worker():
+    print 'worker..'
+    while True:
+        item = WORK_Q.get()
+        do_work(item)
 
-
+def do_work(item):
+    print item
 if __name__=='__main__':
     ##create in a new thread.
-    conn,addr = create_server_socket()
-    conn.send('Wlecome!')
-    time.sleep(5)
-    conn.close()
+    #conn,addr = create_server_socket()
+    WORK_Q.put('test msg')
+    print 'after work q 1'
+    t = Thread(target=worker)
+    t.setDaemon(True)
+    t.start()
+    print 'new thread..'
+    WORK_Q.put('test msg2')
+    print 'after workq2'
