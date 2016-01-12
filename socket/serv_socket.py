c98ce5bca6d1004131d034003869dee40ab96aee
zhibinw <android_zhibinw@163.com>
2016-01-12 22:05:49 +0800

    move serv sock new thread.

--- socket/serv_socket.py
+++ socket/serv_socket.py
@@ -4,26 +4,39 @@ from threading import Thread
 import Queue
 
 WORK_Q = Queue.Queue()
-def create_server_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname()
-    port=2016
-    s.bind((host,port))
-    s.listen(3)
-    #only listen to one 
-    con,addr = s.accept()
-    return con,addr
+is_serv_ready=False
+CONN=None
 def worker():
     print 'worker..'
     while True:
         item = WORK_Q.get()
         do_work(item)
-
+def sock_accept():
+    global is_serv_ready,CONN
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host = socket.gethostname()
+    port=2016
+    s.bind((host,port))
+    s.listen(3)
+    con,addr = s.accept()
+    is_serv_ready = True
+    print "sock accept"
+    CONN=con
 def do_work(item):
-    print item
+    print 'send to client'
+    global CONN
+    CONN.send(item)
 if __name__=='__main__':
     ##create in a new thread.
-    #conn,addr = create_server_socket()
+    t_serv = Thread(target=sock_accept)
+    t_serv.setDaemon(True)
+    t_serv.start()
+    print 'has started'
+    while not is_serv_ready:
+        time.sleep(5)
+        print 'not ready'
+
     WORK_Q.put('test msg')
     print 'after work q 1'
     t = Thread(target=worker)
@@ -32,3 +45,6 @@ if __name__=='__main__':
     print 'new thread..'
     WORK_Q.put('test msg2')
     print 'after workq2'
+    while not WORK_Q.empty():
+        time.sleep(1)
+    print 'task done'
