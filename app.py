48a4b94e88b571f000b53f5518778843213bba49
androidzhibinw <android_zhibinw@163.com>
2016-01-15 10:53:51 +0800

    do not reload prevent app run twice, refer: http://stackoverflow.com/questions/25504149/why-does-running-the-flask-dev-server-run-itself-twice
    use logger to print logs.
    
    Change-Id: I94e2a577d55fefcf30db0d65a683aaffacd08816

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask,render_template,redirect,make_response,request
 from threading import Thread
 import datetime,random,socket,time,Queue
-app = Flask(__name__)
 RANDOM_MIN=100000
 RANDOM_MAX=999999
-
+app = Flask(__name__)
 
 ##for sms service
 WORK_Q = Queue.Queue()
@@ -50,42 +49,45 @@ def enqueue_sms_request(json):
     WORK_Q.put(json)
 
 def sms_worker():
-    print 'sms worker'
+    app.logger.info('sms worker')
     while True:
         if not CONN:
-            print 'not ready'
+            app.logger.info('sms sock connection not ready')
             time.sleep(5)
             continue
         item = WORK_Q.get()
         send_sms(item)
+
 def create_serv_sock():
+    app.logger.info('create_serv_sock start')
     global is_serv_ready,CONN
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     host = socket.gethostname()
-    port=2017
+    port=2016
+    app.logger.info('before bind')
     s.bind((host,port))
+    app.logger.info('after bind')
     s.listen(1)
     con,addr = s.accept()
     is_serv_ready = True
-    print "\nsock accept"
+    app.logger.info('sock accept:' + str(addr))
     CONN=con
+
 def send_sms(msg):
-    print 'send to client',msg
     global CONN
     CONN.send(msg)
+    app.logger.info('send to client:' +msg)
 
 def prepare_sms_service():
     t_serv = Thread(target=create_serv_sock)
     t_serv.setDaemon(True)
     t_serv.start()
-    print 'serv sock has started'
     t_work = Thread(target=sms_worker)
     t_work.setDaemon(True)
     t_work.start()
-    print 'worker thread started'
     WORK_Q.put('test msg 1')
     WORK_Q.put('test msg 2')
 if __name__ == '__main__':
-        prepare_sms_service()
-        app.run(host='0.0.0.0',debug=True,port=8000)
+    prepare_sms_service()
+    app.run(host='0.0.0.0',debug=True,port=8000,use_reloader=False)
