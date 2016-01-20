f3efafcf5e649c598270a4a9d1d576aff90c9735
androidzhibinw <android_zhibinw@163.com>
2016-01-20 16:50:42 +0800

    jquery style js.
    Change-Id: Ife3274c02679ebf0ebec898ccd798140df20837d

--- static/js/reg-jq.js
+++ static/js/reg-jq.js
@@ -1,31 +1,52 @@
 $(document).ready(function (){
 var totalSeconds = 60;
 var seconds = totalSeconds;
-var sendBtn;
+var sendBtn = $('#btn-send');
+var inputPhone = $('#input_phone')
+var inputCode = $('#input_code')
+var loginBtn = $('#btn-login')
+sendBtn.prop('disabled',true)
+
+$('#btn-login').prop('disabled',true)
     $('#btn-send').click(function countdown(){
         if(seconds == totalSeconds) {
             console.log("btn-send clicked");
-            var phoneNumber = $('#phone').val()
+            var phoneNumber = inputPhone.val()
             $.ajax({
                   method: "POST",
                   url: "/getcode",
                   data: JSON.stringify({"number":phoneNumber}),
                   contentType:'application/json;charset=UTF-8'
-            })
-              .done(function( msg ) {
-                      console.log( "Data Saved: " + msg );
-                        });
+            }).done(function( msg ) {
+                 console.log( "getcode response " + msg );
+            });
         }
         if (seconds == 1) {
-          sendBtn.innerHTML = "重新发送";
-          sendBtn.disabled = false
+          sendBtn.html( "重新发送");
+          sendBtn.prop('disabled',false)
           seconds = totalSeconds;
           return;
         }
         seconds--;
-        sendBtn = document.getElementById('btn-send');
-        sendBtn.innerHTML = "重新发送("+seconds+")";
-        sendBtn.disabled=true
+        sendBtn.html("重新发送("+seconds+")");
+        sendBtn.prop('disabled',true)
         timeoutMyOswego = setTimeout(countdown, 1000);
+    });
+    function checkPhone(phone) {
+        var reg = /^1[34578]\d{9}$/;
+        return reg.test(phone);
+    }
+
+    function checkCode(code) {
+        var reg=/^[0-9]{6}$/;
+        return reg.test(code);
+    }
+    $('input[id^="input"]').on("change keyup paste",function inputListener(){
+        console.log("input changed." + inputPhone.val());
+        console.log("input changed." + inputCode.val());
+        var disableBtnSend = !(checkPhone(inputPhone.val()) && (seconds == totalSeconds));
+        sendBtn.prop('disabled',disableBtnSend)
+        var disableBtnLogin = !(checkPhone(inputPhone.val())) || !(checkCode(inputCode.val()));
+        loginBtn.prop('disabled',disableBtnLogin)
     });
 });
