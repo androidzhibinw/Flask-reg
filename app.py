78aa6981cbdadd07297638283fc9cfec5100c8f4
androidzhibinw <android_zhibinw@163.com>
2016-01-21 15:15:29 +0800

    send code via sms: using json data as content. format with autopep8
    Change-Id: I8ea8c8719cd9be89770f50f2c8f14aceeffe8cfa

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask,render_template,redirect,make_response,request
+from flask import Flask, render_template, redirect, make_response, request
 from threading import Thread
-import datetime,random,socket,time,Queue
-RANDOM_MIN=100000
-RANDOM_MAX=999999
+import datetime
+import random
+import socket
+import time
+import Queue
+import json
+RANDOM_MIN = 100000
+RANDOM_MAX = 999999
 app = Flask(__name__)
 
-##for sms service
+# for sms service
 WORK_Q = Queue.Queue()
-is_serv_ready=False
-CONN=None
-END_STR='\n'
+is_serv_ready = False
+CONN = None
+END_STR = '\n'
+
 
 @app.route('/')
 def index():
-    #check cookie, if not set render register
+    # check cookie, if not set render register
     getip()
     username = request.cookies.get('username')
     if not username:
@@ -21,26 +27,29 @@ def index():
     else:
         return render_template('home.html')
 
+
 def genSecCode(number):
-    rand_no=random.randint(RANDOM_MIN,RANDOM_MAX)
+    rand_no = random.randint(RANDOM_MIN, RANDOM_MAX)
     return '%d' % rand_no
+
 
 @app.route('/getcode', methods=['POST'])
 def getcode():
-    #gen security code , insert to db
+    # gen security code , insert to db
     if request.method == 'POST':
         try:
             app.logger.info('getcode pass in:' + str(request.json))
             number = request.json['number']
             if check_phonenumber(number):
-                code = genSecCode(number)
-                print code
+                send_sms4no(number)
             else:
-                return 'fail: number invalid'
+                return 'fail:number invalid'
         except Exception as e:
-            return 'fail', e
+            return str(e)
 
     return 'success'
+
+
 def getip():
     if request.headers.getlist("X-Forwarded-For"):
         ip = request.headers.getlist("X-Forwarded-For")[0]
@@ -48,7 +57,8 @@ def getip():
         ip = request.remote_addr
     print ip
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         data = request.form.to_dict()
@@ -57,16 +67,29 @@ def login():
         #resp = make_response(redirect('/'))
         #expire_date = datetime.datetime.now() + datetime.timedelta(days=365)
         #resp.set_cookie('username', 'myname',expires=expire_date)
-        #return resp
+        # return resp
     return render_template('reg.html')
+
+
+def send_sms4no(number):
+    app.logger.info('send_sms4no:' + str(number))
+    code = genSecCode(number)
+    data = {}
+    data['number'] = number
+    data['code'] = code
+    json_data = json.dumps(data)
+    app.logger.info('sms_json' + str(json_data))
 
 
 def check_phonenumber(number):
     return True
 
-##for sms service
+# for sms service
+
+
 def enqueue_sms_request(json):
     WORK_Q.put(json)
+
 
 def sms_worker():
     app.logger.info('sms worker')
@@ -78,27 +101,30 @@ def sms_worker():
         item = WORK_Q.get()
         send_sms(item)
 
+
 def create_serv_sock():
     app.logger.info('create_serv_sock start')
-    global is_serv_ready,CONN
+    global is_serv_ready, CONN
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #use public ip here.
-    host='127.0.0.1'
-    port=2016
+    # use public ip here.
+    host = '127.0.0.1'
+    port = 2016
     app.logger.info('before bind')
-    s.bind((host,port))
+    s.bind((host, port))
     app.logger.info('after bind')
     s.listen(1)
-    con,addr = s.accept()
+    con, addr = s.accept()
     is_serv_ready = True
     app.logger.info('sock accept:' + str(addr))
-    CONN=con
+    CONN = con
+
 
 def send_sms(msg):
     global CONN
-    CONN.send(msg+END_STR)
-    app.logger.info('send to client:' +msg)
+    CONN.send(msg + END_STR)
+    app.logger.info('send to client:' + msg)
+
 
 def prepare_sms_service():
     t_serv = Thread(target=create_serv_sock)
@@ -111,4 +137,4 @@ def prepare_sms_service():
     WORK_Q.put('test msg 2')
 if __name__ == '__main__':
     prepare_sms_service()
-    app.run(host='0.0.0.0',debug=True,port=8000,use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, port=8000, use_reloader=False)
