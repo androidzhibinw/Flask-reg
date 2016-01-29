4f119465d02538d0002baf5e29d0bfac4f898f4e
androidzhibinw <android_zhibinw@163.com>
2016-01-29 16:20:55 +0800

    reconnect for client
    Change-Id: I9268c5f2af8f52391314d985131d41b582f960b0

--- socket/client_socket.py
+++ socket/client_socket.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 
-import socket 
+import socket
+import time
+
+HOST = socket.gethostname()
+PORT = 2016
+SOCK_RETRY_SEC = 10
+BUFF_SIZE = 1024
+
 
 def local_socket():
-    s = socket.socket()
-    host = '127.0.0.1'
-    port=2016
-    s.connect((host,port))
-    #should have threads for handle request
+    s = try_to_connect()
+    # should have threads for handle request
     while True:
-        msg=s.recv(1024)
-        ## handle msg in a thread
-        handle_msg(msg)
+        msg = s.recv(BUFF_SIZE)
+        # handle msg in a thread
+        if len(msg) == 0:
+            s = try_to_connect()
+        else:
+            handle_msg(msg)
+
 
 def handle_msg(msg):
     print msg
 
+
+def try_to_connect():
+    s = socket.socket()
+    while True:
+        try:
+            s.connect((HOST, PORT))
+            return s
+        except Exception as e:
+            print e
+            time.sleep(SOCK_RETRY_SEC)
+
+
 if __name__ == '__main__':
     local_socket()
-
